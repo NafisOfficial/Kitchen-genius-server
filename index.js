8cc6f23e821a079f084902db176014d0f708a9f2
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-05-04 19:46:23 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
-const chefs = require('./data/chef.json')
+const chefs = require('./data/chef.json');
 
 
+app.use(cors());
 
 
 app.get('/chefs', (req, res) => {
   res.send(chefs)
-})
+});
 
 
 app.get('/chefs/:id', (req, res) => {
-  const Id = parseInt(req.params.id);
-  console.log(Id);
-  const selectedChef = chefs.find(n=>n.chefId===Id)
+  const id = parseInt(req.params.id);
+  const selectedChef = chefs.find(n=>n.chefId===id)
   res.send(selectedChef);
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
